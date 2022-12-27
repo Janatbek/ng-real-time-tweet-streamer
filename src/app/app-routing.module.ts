3a017ae3e21661635188c74409a8e4e1475e33f3
Janatbek Sharsheyev <janatbek@gmail.com>
2022-12-27 03:40:34 -0700

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RulesListComponent } from './components/rules-list/rules-list.component';
+import { TweetFeedComponent } from './components/tweet-feed/tweet-feed.component';
+import { TweetComponent } from './components/tweet/tweet.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: RulesListComponent},
+  {path:'rules', component: RulesListComponent},
+  {path:'tweets', component: TweetFeedComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { TweetComponent } from './components/tweet/tweet.component';
+import { TweetFeedComponent } from './components/tweet-feed/tweet-feed.component';
+import { RuleComponent } from './components/rule/rule.component';
+import { RulesListComponent } from './components/rules-list/rules-list.component';
+import { ErrorMessageComponent } from './components/error-message/error-message.component';
+import { SpinnerComponent } from './components/spinner/spinner.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    TweetComponent,
+    TweetFeedComponent,
+    RuleComponent,
+    RulesListComponent,
+    ErrorMessageComponent,
+    SpinnerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
